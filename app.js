90f1c395b7029f2d20fcbb6f65cb3995be7ef7c4
kimvietanh <anhkv@sphinx-software.com>
2019-05-07 17:05:05 +0700

    viet anh commit

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const Koa = require('koa');
-const nunjuck = require('./nunjucks.provider');
+const Koa         = require('koa');
+const nunjuck     = require('./nunjucks.provider');
 const routerLogin = require('./router/RouterLogin');
+const serve       = require('koa-static');
+const path        = require('path');
+const staticPath  = '/views/Admin';
 
 const app = new Koa();
+app.use(serve(
+    path.join(__dirname, staticPath)
+));
 
 app.use(nunjuck());
 app.use(routerLogin.routes());
-app.listen(6060);
+app.listen(6060, () => {
+    console.log('Server listen port 6060')
+});
--- controller/LoginController.js
+++ controller/LoginController.js
@@ -3,7 +3,7 @@ const knex = require('knex')(KnexFile.development);
 
 class LoginController {
     async getLogin(ctx, next) {
-        ctx.render('login.html', true);
+        ctx.render('login.html');
     }
 }
 
